755e43cddc9b98a5358760a34f5936373abe7519
cia76 <ichechet@mail.ru>
2025-04-17 13:24:45 +0500

    Отображение графика в Lightweight Chart от TradingView

---
+++ Brokers/Examples/BarsTradingView.py
@@ -0,0 +1,20 @@
+from lightweight_charts import Chart  # pip install lightweight-charts
+
+from FinLabPy.Config import brokers, default_broker  # Все брокеры и брокер по умолчанию
+
+
+if __name__ == '__main__':  # Точка входа при запуске этого скрипта
+    dataname = 'TQBR.SBER'
+    time_frame = 'D1'
+
+    broker = default_broker  # Брокер по умолчанию
+    # broker = brokers['Т']  # Брокер по ключу из Config.py словаря brokers
+    bars = broker.get_history(dataname, time_frame)  # Получаем всю историю тикера
+    pd_bars = broker.bars_to_df(bars)  # Бары в pandas DataFrame
+
+    chart = Chart(toolbox=True)  # График с элементами рисования
+    chart.legend(True)  # В верхнем левом углу отображаются значения по указателю мыши
+    chart.set(pd_bars)  # Отправляем бары на график
+    chart.show(block=True)  # Отображаем график. Блокируем дальнейшее исполнение кода, пока его не закроем
+
+    broker.close()  # Закрываем брокера
