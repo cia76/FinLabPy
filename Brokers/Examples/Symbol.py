a9ccbc50922a32982d1cd2f65a9ff942a3f1079e
cia76 <ichechet@mail.ru>
2025-05-28 12:38:42 +0500

    Спецификация тикера для всех брокеров

--- Brokers/Examples/Symbol.py
+++ Brokers/Examples/Symbol.py
@@ -1,10 +1,9 @@
-from FinLabPy.Config import brokers, default_broker  # Все брокеры и брокер по умолчанию
+from FinLabPy.Config import brokers  # Все брокеры
 
 
 if __name__ == '__main__':  # Точка входа при запуске этого скрипта
     dataname = 'TQBR.SBER'
 
-    broker = default_broker  # Брокер по умолчанию
-    # broker = brokers['Т']  # Брокер по ключу из Config.py словаря brokers
-    print(broker.get_symbol_by_dataname(dataname))  # Тикер по названию
-    broker.close()  # Закрываем брокера
+    for code, broker in brokers.items():  # Пробегаемся по всем брокерам
+        print(f'[{code}] {broker.get_symbol_by_dataname(dataname)}')  # Спецификация тикера брокера. Должна совпадать у всех брокеров
+        broker.close()  # Закрываем брокера
