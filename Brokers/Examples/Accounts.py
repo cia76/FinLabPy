a19795716e0b4e17183f4682f6967d20abe5e367
cia76 <ichechet@mail.ru>
2025-10-18 23:14:15 +0500

    Оформление

--- Brokers/Examples/Accounts.py
+++ Brokers/Examples/Accounts.py
@@ -4,11 +4,15 @@ from FinLabPy.Config import brokers  # Все брокеры
 if __name__ == '__main__':  # Точка входа при запуске этого скрипта
     for code, broker in brokers.items():  # Пробегаемся по всем брокерам
         print(f'[{code}] {broker.name}')
-        print(f'Стоимость позиций : {broker.get_value()}')
-        print(f'Свободные средства: {broker.get_cash()}')
+        print('- Позиции:')
         for position in broker.get_positions():  # Пробегаемся по всем позициям брокера
-            print(position)
+            print(f'  - {position}')
+        value = broker.get_value()  # Стоимость позиций
+        print(f'- Стоимость позиций  : {value}')
+        print('- Заявки:')
         for order in broker.get_orders():  # Пробегаемся по всем заявкам брокера
-            print(order)
-    for broker in brokers.values():  # Пробегаемся по всем брокерам
+            print(f'  - {order}')
+        cash = broker.get_cash()  # Свободные средств
+        print(f'- Свободные средства : {cash}')
+        print(f'- Итого              : {value + cash}')
         broker.close()  # Закрываем брокера
