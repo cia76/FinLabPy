d4a00e2a8ce17b31b1f8b950eee5417402713386
cia76 <ichechet@mail.ru>
2025-10-18 23:10:55 +0500

    FinamPyOld -> FinamPy, TinkoffPy -> TinvestPy

--- Config.py
+++ Config.py
@@ -3,11 +3,11 @@
 from AlorPy import AlorPy  # Провайдер Алор
 from FinLabPy.Brokers.Alor import Alor  # Брокер Алор
 
-from FinamPy import FinamPyOld  # Провайдер Финам
-from FinLabPy.Brokers.FinamOld import Finam  # Брокер Финам
+from FinamPy import FinamPy  # Провайдер Финам
+from FinLabPy.Brokers.Finam import Finam  # Брокер Финам
 
-from TinkoffPy import TinkoffPy  # Провайдер Т-Инвестиции
-from FinLabPy.Brokers.Tinkoff import Tinkoff  # Брокер Т-Инвестиции
+from TinvestPy import TinvestPy  # Провайдер Т-Инвестиции
+from FinLabPy.Brokers.Tinvest import Tinvest  # Брокер Т-Инвестиции
 
 # from QuikPy import QuikPy  # Провайдер QUIK
 # from FinLabPy.Brokers.Quik import Quik  # Брокер QUIK
@@ -15,8 +15,8 @@ from FinLabPy.Brokers.Tinkoff import Tinkoff  # Брокер Т-Инвестиц
 
 # Провайдеры
 ap_provider = AlorPy()  # Провайдер Алор. Для демо счета AlorPy(demo=True)
-fp_provider = FinamPyOld()  # Провайдер Финам
-tp_provider = TinkoffPy()  # Провайдер Т-Инвестиции. Для демо счета TinkoffPy(demo=True). Курс Мультиброкер: Облигации https://finlab.vip/wpm-category/mbbonds/
+fp_provider = FinamPy()  # Провайдер Финам
+tp_provider = TinvestPy()  # Провайдер Т-Инвестиции. Для демо счета TinkoffPy(demo=True). Курс Мультиброкер: Облигации https://finlab.vip/wpm-category/mbbonds/
 # qp_provider = QuikPy()  # Провайдер QUIK
 
 # Брокеры
@@ -30,7 +30,7 @@ brokers = {
     'ИФ': Alor(code='ИФ', name='Алор ИИС - Фондовый рынок', provider=ap_provider, account_id=4, storage=storage),  # Алор ИИС - Портфель фондового рынка
     # 'ИВ': Alor(code='ИВ', name='Алор ИИС - Валютный рынок', provider=ap_provider, account_id=5, storage=storage),  # Алор ИИС - Портфель валютного рынка
     'Ф': Finam(code='Ф', name='Финам', provider=fp_provider, storage=storage),  # Финам
-    'Т': Tinkoff(code='Т', name='Т-Инвестиции', provider=tp_provider, storage=storage),  # Т-Инвестиции
+    'Т': Tinvest(code='Т', name='Т-Инвестиции', provider=tp_provider, storage=storage),  # Т-Инвестиции
     # 'КС': Quik(code='КС', name='QUIK - Срочный рынок', provider=qp_provider, account_id=0, storage=storage),  # QUIK - Портфель срочного рынка
     # 'КФ': Quik(code='КФ', name='QUIK - Фондовый рынок', provider=qp_provider, account_id=1, storage=storage),  # QUIK - Портфель фондового рынка
     # 'КВ': Quik(code='КВ', name='QUIK - Валютный рынок', provider=qp_provider, account_id=2, storage=storage),  # QUIK - Портфель валютного рынка
