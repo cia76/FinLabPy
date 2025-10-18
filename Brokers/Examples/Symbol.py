d917b23db649d9af9443f448f6af4393ef1e8f93
cia76 <ichechet@mail.ru>
2025-10-18 23:35:27 +0500

    Информация брокера

--- Brokers/Examples/Symbol.py
+++ Brokers/Examples/Symbol.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':  # Точка входа при запуске это
     dataname = 'TQBR.SBER'
 
     for code, broker in brokers.items():  # Пробегаемся по всем брокерам
-        print(f'[{code}] {broker.get_symbol_by_dataname(dataname)}')  # Спецификация тикера брокера. Должна совпадать у всех брокеров
+        symbol = broker.get_symbol_by_dataname(dataname)  # Спецификация тикера брокера. Должна совпадать у всех брокеров
+        print(f'[{code}] {symbol} Информация брокера: {symbol.broker_info}')
         broker.close()  # Закрываем брокера
