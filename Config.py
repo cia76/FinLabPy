ea9bf63a6efe869293644bf908201ff2e0d74893
cia76 <ichechet@mail.ru>
2025-11-14 10:12:54 +0500

    Вызов библиотеки from <Пакет>.<Модуль> import <Библиотека>. Вторые счета Финам/Т-Инвестиции

--- Config.py
+++ Config.py
@@ -1,15 +1,15 @@
 # Курс Мультиброкер: Контроль https://finlab.vip/wpm-category/mbcontrol/
 
-from AlorPy import AlorPy  # Провайдер Алор
+from AlorPy.AlorPy import AlorPy  # Провайдер Алор
 from FinLabPy.Brokers.Alor import Alor  # Брокер Алор
 
-from FinamPy import FinamPy  # Провайдер Финам
+from FinamPy.FinamPy import FinamPy  # Провайдер Финам
 from FinLabPy.Brokers.Finam import Finam  # Брокер Финам
 
-from TinvestPy import TinvestPy  # Провайдер Т-Инвестиции
+from TinvestPy.TinvestPy import TinvestPy  # Провайдер Т-Инвестиции
 from FinLabPy.Brokers.Tinvest import Tinvest  # Брокер Т-Инвестиции
 
-# from QuikPy import QuikPy  # Провайдер QUIK
+# from QuikPy.QuikPy import QuikPy  # Провайдер QUIK
 # from FinLabPy.Brokers.Quik import Quik  # Брокер QUIK
 
 # Провайдеры
@@ -29,7 +29,9 @@ brokers = {
     # 'ИС': Alor(code='ИС', name='Алор ИИС - Срочный рынок', provider=ap_provider, account_id=3, storage=storage),  # Алор ИИС - Портфель срочного рынка
     # 'ИВ': Alor(code='ИВ', name='Алор ИИС - Валютный рынок', provider=ap_provider, account_id=5, storage=storage),  # Алор ИИС - Портфель валютного рынка
     'Ф': Finam(code='Ф', name='Финам', provider=fp_provider, storage=storage),  # Финам
+    # 'Ф2': Finam(code='Ф', name='Финам', provider=fp_provider, account_id=1, storage=storage),  # Второй счет на Финам
     'Т': Tinvest(code='Т', name='Т-Инвестиции', provider=tp_provider, storage=storage),  # Т-Инвестиции
+    # 'Т2': Tinvest(code='Т', name='Т-Инвестиции', provider=tp_provider, account_id=1, storage=storage),  # Второй счет на Т-Инвестиции
     # 'КФ': Quik(code='КФ', name='QUIK - Фондовый рынок', provider=qp_provider, account_id=0, storage=storage),  # QUIK - Портфель фондового рынка
     # 'КС': Quik(code='КС', name='QUIK - Срочный рынок', provider=qp_provider, account_id=1, storage=storage),  # QUIK - Портфель срочного рынка
     # 'КВ': Quik(code='КВ', name='QUIK - Валютный рынок', provider=qp_provider, account_id=2, storage=storage),  # QUIK - Портфель валютного рынка
