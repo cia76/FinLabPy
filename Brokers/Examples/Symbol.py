08b43947fb568cdaa55952c85c9b443466aa24e1
cia76 <ichechet@mail.ru>
2025-11-19 20:26:39 +0500

    Отдельный вызов для закрытия брокероу

--- Brokers/Examples/Symbol.py
+++ Brokers/Examples/Symbol.py
@@ -7,4 +7,5 @@ if __name__ == '__main__':  # Точка входа при запуске это
     for code, broker in brokers.items():  # Пробегаемся по всем брокерам
         symbol = broker.get_symbol_by_dataname(dataname)  # Спецификация тикера брокера. Должна совпадать у всех брокеров
         print(f'[{code}] {symbol} Информация брокера: {symbol.broker_info}')
+    for _, broker in brokers.items():  # Пробегаемся по всем брокерам. Отдельный вызов нужен из-за того, что счета брокеров могут относится к одному брокеру
         broker.close()  # Закрываем брокера
